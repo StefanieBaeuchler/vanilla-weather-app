d007b6fcb92c8a85b40a2f422dbc98e8b7738800
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2022-10-12 07:33:03 +0200

    Removed HTML and injected it into JS

--- src/index.js
+++ src/index.js
@@ -21,6 +21,13 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
@@ -43,6 +50,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -56,6 +65,42 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["We", "Thu", "Fri"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+              
+                <div class="col-2">
+                  <div class="weather-forecast-date">
+                  ${day} 
+                  </div>
+              
+                   <img
+                  src="http://openweathermap.org/img/wn/09n@2x.png"
+                  alt="" width="36"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">
+                18° 
+                </span>
+                <span class="weather-forecast-temperature-min">
+                12°
+                </span>
+                </div>
+                
+              </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayFahrenheitTemperature(event) {
@@ -87,3 +132,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Vienna");
+displayForecast();
