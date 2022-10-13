0d8cf4fcec9e5e6f9d6093e9d74de659edf5ab5d
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2022-10-13 06:50:05 +0200

    Display the forecast data using the API

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function getForecast(coordinates) {
   let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -68,34 +76,40 @@ function handleSubmit(event) {
 }
 
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["We", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
               
                 <div class="col-2">
                   <div class="weather-forecast-date">
-                  ${day} 
+                  ${formatDay(forecastDay.dt)} 
                   </div>
               
                    <img
-                  src="http://openweathermap.org/img/wn/09n@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt="" width="36"
                 />
                 <div class="weather-forecast-temperatures">
                   <span class="weather-forecast-temperature-max">
-                18° 
+                ${Math.round(forecastDay.temp.max)}° 
                 </span>
                 <span class="weather-forecast-temperature-min">
-                12°
+               ${Math.round(forecastDay.temp.min)}°
                 </span>
                 </div>
                 
               </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
